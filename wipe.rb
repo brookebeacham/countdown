dffd79da2ecff09b1e6e97bf028d7efa693dc7eb
Brooke Beacham <brooke.beacham@technekes.com>
2018-12-09 18:12:43 -0500

    wip

--- wipe.rb
+++ wipe.rb
@@ -3,6 +3,10 @@ $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'ws2812'
 
 n = 50 # total num leds
+ws = Ws2812::Basic.new(n, 18) # +n+ leds at pin 18, using defaults
+ws.open
+
 black = Ws2812::Color.new(0, 0, 0)
 ws[(0...n-1)] = black
 ws.show
+ws.close
