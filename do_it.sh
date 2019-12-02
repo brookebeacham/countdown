adee3de034168c0ddc96e077d5d049767a8cf3ec
Brooke Beacham <brooke.beacham@technekes.com>
2019-12-01 21:06:55 -0500

    adds execution script and razzle dazzle py script

---
+++ do_it.sh
@@ -0,0 +1,6 @@
+export PATH=/root/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+eval "$(rbenv init -)"
+#git pull origin test
+ruby countdown.rb
+python strandtest.py -c
+ruby wipe.rb
